4e1995ae5212825e75f8e2355d7d81c646eeeca2
Felix Fernando <felixzhuang1@gmail.com>
2023-03-01 23:17:13 +0700

    feat: added function boiler plate

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,12 @@ function App() {
             });
     };
 
+    const addCustomerData = () => {};
+
+    const addStockData = () => {};
+
+    const addTransaction = () => {};
+
     const trainModel = () => {
         setIsTraining(true);
         axios
@@ -172,7 +178,7 @@ function App() {
                                     required
                                 />
                                 <button
-                                    onClick={() => getRecommendation()}
+                                    onClick={() => addCustomerData()}
                                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-50"
                                     disabled={isLoading}
                                 >
@@ -219,7 +225,7 @@ function App() {
                                 </div>
                             </div>
                             <button
-                                onClick={() => getRecommendation()}
+                                onClick={() => addStockData()}
                                 className=" mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-50"
                                 disabled={isLoading}
                             >
@@ -284,7 +290,7 @@ function App() {
                                 </div>
                             </div>
                             <button
-                                onClick={() => getRecommendation()}
+                                onClick={() => addTransaction()}
                                 className=" mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-50"
                                 disabled={isLoading}
                             >
